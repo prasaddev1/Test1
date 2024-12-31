c76fe4e50b59d1a5a2f431bdc8c24e2266f450eb
prasaddev1 <jagtapprasad7777@gmail.com>
2024-12-31 14:34:36 +0530

    trial code added

---
+++ example.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+public class example {
+    public static void main(String[] args) {
+        JFrame a = new JFrame("example");
+        JTextField a = new JTextField("Welvome to Java");
+        b.SetBounds(50,100,200,30);
+        a.add(b);
+        a.setSize(300,300);
+        a.SetLayout(null);
+        a.SetVisible(True);
+    }
+    
+}
